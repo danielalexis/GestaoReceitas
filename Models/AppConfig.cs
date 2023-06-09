bd9b03e847d07dd9e71227de7f215b6df5c0da35
danielalexis <danielalexis.o.pereira@gmail.com>
2023-06-09 16:49:01 +0100

    inital commit

---
+++ Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace GestaoReceitas.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
