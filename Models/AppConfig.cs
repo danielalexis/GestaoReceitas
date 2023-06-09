4b835855606e0d7ac1b037590388a468196e5eca
danielalexis <danielalexis.o.pereira@gmail.com>
2023-06-09 17:07:41 +0100

    removal

--- Models/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestaoReceitas.Models
-{
-    public class AppConfig
-    {
-        public string ConfigurationsFolder { get; set; }
-
-        public string AppPropertiesFileName { get; set; }
-    }
-}
